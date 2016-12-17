69ad003b14293dcab5ae319976447e70e003c89c
Andrew Bagwell <andrewbagwell1@gmail.com>
2016-12-16 21:54:12 -0600

    altered index template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+
 
 def index(request):
 	#construct dict to pass to the template enginge as its context - the key is bold message and is the same as {{bold message}} from the template
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-	#return the rendered response to the client, the first parameter is the template we want to use
-	return render(request, 'rango/index.html', context=context_dict)
+    
+    category_list = Category.objects.order_by('likes')[:5]
+
+    context_dict = {'categories': category_list}
+	
+    #return the rendered response to the client, the first parameter is the template we want to use
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'aboutMessage': 'I have added this message using a view!'}
