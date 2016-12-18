cfa3e7b6efb2209dedc4ea4cb685a3293e1ac64f
Andrew Bagwell <andrewbagwell1@gmail.com>
2016-12-17 20:17:13 -0600

    added slug urls and updated index page to conform with specifications

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -9,6 +10,10 @@ def index(request):
     category_list = Category.objects.order_by('likes')[:5]
 
     context_dict = {'categories': category_list}
+    
+    view_list = Page.objects.order_by('views')[:5]
+    
+    context_dict['pages'] = view_list
 	
     #return the rendered response to the client, the first parameter is the template we want to use
 
@@ -17,5 +22,22 @@ def index(request):
 def about(request):
     context_dict = {'aboutMessage': 'I have added this message using a view!'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 
 
